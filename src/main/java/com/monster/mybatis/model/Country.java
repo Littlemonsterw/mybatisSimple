eebd1cc331d548bfe0d20c1eec473493308285a7
wuhan <wuhan@lecent.cn>
2019-12-05 15:39:37 +0800

    MyBatis入门

---
+++ src/main/java/com/monster/mybatis/model/Country.java
@@ -0,0 +1,36 @@
+package com.monster.mybatis.model;
+
+/**
+ * @Author wuhan
+ * @Date 2019/12/5 14:20
+ */
+public class Country {
+
+    private Long id;
+    private String countryname;
+    private String countrycode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCountryname() {
+        return countryname;
+    }
+
+    public void setCountryname(String countryname) {
+        this.countryname = countryname;
+    }
+
+    public String getCountrycode() {
+        return countrycode;
+    }
+
+    public void setCountrycode(String countrycode) {
+        this.countrycode = countrycode;
+    }
+}
---
+++ src/test/java/com/monster/mybatis/AppTest.java
@@ -0,0 +1,20 @@
+package com.monster.mybatis;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
